70f09d4ec93cee7ceebae4213a02eab75d3d44e4
Beibala <ggbeibala05@gmail.com>
2025-02-22 20:38:30 +0500

    fix lab_3

--- lab_3.m
+++ lab_3.m
@@ -6,7 +6,6 @@ x2 = x1;
 f1 = cos(x1 + 0.5) + x2 - 0.8;
 f2 = -2 .* x1 + sin(x2) - 1.6;
 
-
 % 1.1
 g1 = acos(0.8 - x2) + 0.5;
 g2 = (1.6 - sin(x2)) ./ (-2);
@@ -28,7 +27,7 @@ phi2 = (1.6 - sin(x2)) ./ (-2);
 x1_0 = 0;
 x2_0 = 0;
 
-for k = 1:100
+while true
     x1_next = (1.6 - sin(x2_0)) ./ (-2);
     x2_next = -cos(x1_0 + 0.5) + 0.8;
 
@@ -38,17 +37,76 @@ for k = 1:100
 
     x1_0 = x1_next;
     x2_0 = x2_next;
-end
+endwhile
 
 fprintf('Сошлось к (%f, %f)\n', x1_0, x2_0);
-fprintf('\n\n');
+
+x_0 = [-1, 1];
 
 J = [ 
-        -sin(x1 + 0.5),  1
-        -2            ,  cos(x2) 
+        -sin(x_0(1) + 0.5), 0
+        0                 , cos(x_0(2)) / 2 
     ];
+    
 q = norm(J);
-
 
 % 1.4
 eps_0 = ((1 - q) / q) * eps;
+
+printf("\n\n___________________________________________________________\n");
+printf("| n |    x1     |     x2    |   difference     | epsilon_0 |\n");
+
+k1 = 0;
+x_prev = x_0;
+
+while true
+
+    k1 += 1;
+    x_next = [-cos(x_prev(1) + 0.5) + 0.8, (1.6 - sin(x_prev(2))) / (-2)];
+    difference = norm(x_next - x_prev);
+    x_prev = x_next;
+
+    if (difference <= eps_0)
+        printf("| %d | %f | %f |   %f       | %f  |\n", k1, x_next(1), x_next(2), difference, eps_0);
+        break;
+    end
+
+    printf("| %d | %f | %f |   %f       | %f  |\n", k1, x_next(1), x_next(2), difference, eps_0);
+endwhile
+printf("___________________________________________________________\n");
+
+% 1.5
+printf("Номер итерации на которой была достигнута заданная точность: %d\n", k1);
+
+
+% 2.1
+printf("\n\n___________________________________________________________\n");
+printf("| n |    x1     |     x2    |   difference     | epsilon_0 |\n");
+
+x_prev = x_0;
+k2 = 0;
+
+while true
+    k2 += 1;
+
+    eq_1 = x_prev;
+    eq_2 = [-sin(x_prev(1) + 0.5), 1; -2, cos(x_prev(2))];
+    eq_3 = [cos(x_prev(1) + 0.5) + x_prev(2) - 0.8; -2 * x_prev(1) + sin(x_prev(2)) - 1.6];
+
+    x_next = eq_1 - inv(eq_2) * eq_3;
+    difference = norm(x_next - x_prev);
+    x_prev = x_next;
+
+    if (difference <=  eps)
+        printf("| %d | %f | %f |   %f       | %f  |\n", k2, x_next(1), x_next(2), difference, eps);
+        break
+    end
+
+    printf("| %d | %f | %f |   %f       | %f  |\n", k2, x_next(1), x_next(2), difference, eps);
+
+endwhile
+printf("___________________________________________________________\n");
+
+
+% 2.2
+printf("Номер итерации на которой была достигнута заданная точность: %d\n", k2);
